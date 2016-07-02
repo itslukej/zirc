65dfe09350d911d5113c7cad9720d1de4b3a1173
itslukej <me@lukej.me>
2016-07-02 18:54:47 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='zirc',
+      version='0.0.2',
+      description="Python IRCP Library",
+      url='https://github.com/itslukej/zirc',
+      author='Luke J.',
+      author_email='me+zirc@lukej.me',
+      license='GNL',
+      packages=["zirc"],
+      install_requires=['six'],
+      include_package_data=True,
+      zip_safe=False)
