3c31960e77b85ec7bbd298f434e3846bf9b09f28
itslukej <me@lukej.me>
2016-07-08 14:45:47 +0100

    Update travis.py

--- tests/travis.py
+++ tests/travis.py
@@ -17,7 +17,7 @@ class Bot(zirc.Client):
         time.sleep(1)
         irc.send("QUIT :My work here is done")
         time.sleep(1)
-        exit()
+        sys.exit()
 
     def on_all(irc, event):
         print(event)
