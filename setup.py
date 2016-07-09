f78d4012ab3f4b7729ab864e361a35e3859480fe
itslukej <me@lukej.me>
2016-07-09 15:38:15 +0000

    Change version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='zirc',
-      version='1.0.1',
+      version='1.0.2',
       description="Python IRCP Library",
       url='https://github.com/itslukej/zirc',
       author='Luke J.',
