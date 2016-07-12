67b5de2d0b05d68b2c5a660c0ef0aebf783d7b3a
itslukej <me@lukej.me>
2016-07-12 22:07:20 +0100

    Update travis.py

--- tests/travis.py
+++ tests/travis.py
@@ -3,8 +3,8 @@ import zirc, ssl, time, sys, os
 class Bot(zirc.Client):
     def __init__(self):
         self.connection = zirc.Socket(wrapper=ssl.wrap_socket)
-        self.connect(address="irc.stuxnet.xyz", port=6697, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[])
-
+        self.config = IRCConfig(host="irc.stuxnet.xyz", port=6697, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[])
+        self.connect(self.config)
         self.start()
 
     def on_welcome(irc, event):
