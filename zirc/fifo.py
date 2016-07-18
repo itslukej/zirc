ecb663a413201756dd476420d3fa212c2e0325e3
itslukej <me@lukej.me>
2016-07-18 21:08:00 +0000

    Add fifo module

---
+++ zirc/fifo.py
@@ -0,0 +1,15 @@
+import os, threading
+
+def fifo_while(send_function, name):
+    if not os.path.exists(name):
+        os.mkfifo(name)
+    with open(name, "r") as file:
+        while True:
+            line = file.readline()[:-1]
+            if len(line) > 0:
+                send_function(line)
+                
+def fifo(send_function, name="fifo"):
+    thread = threading.Thread(target=fifo_while, args=(send_function, name))
+    thread.daemon = True
+    thread.start()
