379e860cd5491b059a6251d6939f63851f92b917
itslukej <me@lukej.me>
2016-07-25 17:04:10 +0000

    Add proxy extension

---
+++ zirc/ext/proxy.py
@@ -0,0 +1,17 @@
+import socks
+
+SOCKS5 = socks.SOCKS5
+SOCKS4 = socks.SOCKS4
+HTTP = socks.HTTP
+
+class Proxy(socks.socksocket):
+    def __init__(self, host="localhost", port=1080, protocol=socks.SOCKS5):
+        socks.socksocket.__init__(self)
+        self.set_proxy(protocol, host, port)
+        
+        self.host = host
+        self.port = port
+        self.protocol = protocol
+    def __repr__(self):
+        return "Proxy({0}, {1})".format(self.host, self.protocol)
+
