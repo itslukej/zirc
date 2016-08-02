db58cf9cda2605cade43a79779dbbd01379980b5
itslukej <me@lukej.me>
2016-08-02 18:26:37 +0000

    Make threading optional

--- zirc/util.py
+++ zirc/util.py
@@ -1,13 +1,16 @@
 import inspect, threading
 
-def function_argument_call(func, arguments):
+def function_argument_call(func, arguments, do_thread=True):
     accepts = inspect.getargspec(func)[0]
     x = {}
     for val, arg in enumerate(accepts):
         if val == 0 and inspect.ismethod(func):
             continue #Ingnore first argument if it is a method
         x[arg] = arguments.get(arg, None)
-    #call_func = lambda: func(**x)
-    thread = threading.Thread(target=func, kwargs=x)
-    thread.daemon = True
-    return thread.start
+    if do_thread:
+        thread = threading.Thread(target=func, kwargs=x)
+        thread.daemon = True
+        return thread.start
+    call_func = lambda: func(**x)
+    return call_func
+    
