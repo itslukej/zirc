02e4e74b693c0995a629c92c45b7139ba7de1480
itslukej <me@lukej.me>
2016-08-08 16:28:26 +0000

    Allow class use with function_argument_call

--- zirc/util.py
+++ zirc/util.py
@@ -1,10 +1,13 @@
 import inspect, threading
 
 def function_argument_call(func, arguments, do_thread=True):
-    accepts = inspect.getargspec(func)[0]
+    try:
+        accepts = inspect.getargspec(func)[0]
+    except TypeError:
+        accepts = inspect.getargspec(func.__init__)[0]
     x = {}
     for val, arg in enumerate(accepts):
-        if val == 0 and inspect.ismethod(func):
+        if val == 0 and (inspect.ismethod(func) or inspect.isclass(func)):
             continue #Ingnore first argument if it is a method
         x[arg] = arguments.get(arg, None)
     if do_thread:
@@ -13,4 +16,3 @@ def function_argument_call(func, arguments, do_thread=True):
         return thread.start
     call_func = lambda: func(**x)
     return call_func
-    
