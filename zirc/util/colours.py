092bc6c159358f340ea061269ea1073b764dd612
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-10-22 15:36:21 -0400

    Remove the rainbow function, plus a small fix
    return coloured and not returned

--- zirc/util/colours.py
+++ zirc/util/colours.py
@@ -27,21 +27,18 @@ def getColour(colour):
 
 def colour(string, c):
     if c == 'rainbow':
-        rainbow(string)
+        i = 0
+        coloured = ""
+
+        for character in string:
+            if i > (len(rainbow) - 1):  # We substract one because i starts at 0 and len(rainbow) at 1
+                i = 0
+
+            coloured += "\x03{0}{1}".format(getColour(rainbow[i]), character)
+            i += 1
+
+        return coloured + "\x0F"
     elif c is None:
         return string
     else:
-        return "\x03{}{}\x0F".format(getColour(c), string)
-
-def rainbow(string):
-    i = 0
-    coloured = ""
-
-    for character in string:
-        if i > (len(rainbow) - 1):  # We substract one because i starts at 0 and len(rainbow) at 1
-            i = 0
-
-        coloured += "\x03{0}{1}".format(getColour(rainbow[i]), character)
-        i += 1
-
-    return returned + "\x0F"
+        return "\x03{}{}\x0F".format(getColour(c), string)   
