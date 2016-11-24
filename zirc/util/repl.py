c79b139b940739d7902d572e9043fbe17602677d
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-11-24 11:51:37 -0500

    Fix call to super

--- zirc/util/repl.py
+++ zirc/util/repl.py
@@ -5,7 +5,7 @@ class Repl(code.InteractiveConsole):
     def __init__(self, items=None):
         if items is None:
             items = {}
-        super(code.InteractiveConsole, self).__init__(items)
+        super(Repl, self).__init__(items)
         self._buffer = ""
 
     def write(self, data):
