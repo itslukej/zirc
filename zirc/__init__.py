fd88f1eff677d701f192180365ee344db0384f6b
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-11-25 13:32:08 -0500

    Avoid using wildcard (*) imports

--- zirc/__init__.py
+++ zirc/__init__.py
@@ -2,6 +2,6 @@ from .client import Client
 from .connection import Socket
 from .config import IRCConfig
 from .ext.sasl import Sasl
-from .ext.proxy import *
+from .ext.proxy import SOCKS4, SOCKS5, HTTP, Proxy
 from .ext.fifo import Fifo
 from .caps import Caps
