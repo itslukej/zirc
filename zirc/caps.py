itslukej/zirc

commit d9a1b3d2ef184910924fca363d6675868fb36eb4

Make the code look cleaner for caps
Also allow us to add the `NEW` and `DEL` sub-commands

m

zirc/caps.py
@@ -12,25 +12,25 @@ class Caps(object):
                 self.stringcaps.append(cap)
 
     def handler(self, event):
-        if not self.done:
-            if event.arguments[0] == "LS":
-                servcaps = event.arguments[1].split(' ')
-                for c in servcaps:
-                    if c.split("=")[0] in self.stringcaps:
-                        self.availablecaps.append(c)
-                        if c.find('=') != -1:
-                            c, args = c.split('=')
-                            self.args[c] = args.split(',')
-                        else:
-                            self.args[c] = None
+        if event.arguments[0] == "LS" and not self.done:
+            servcaps = event.arguments[1].split(' ')
+            for c in servcaps:
+                cap = c.split("=")[0]
+                if cap in self.stringcaps:
+                    self.availablecaps.append(cap)
+                    if c.find('=') != -1:
+                        args = c.split('=')[1]
+                        self.args[cap] = args.split(',')
+                    else:
+                        self.args[cap] = None
                 if not self.availablecaps:
                     self.bot.send("CAP END")
                 else:
                     self.bot.send("CAP REQ :" + " ".join(self.availablecaps))
-            elif event.arguments[0] == "ACK":
-                for cap in self.caps:
-                    if hasattr(cap, "run"):
-                        cap.run(self.bot, args=self.args[cap])
+        elif event.arguments[0] == "ACK" and not self.done:
+            for cap in self.caps:
+                if hasattr(cap, "run"):
+                    cap.run(self.bot, args=self.args[cap])
                 self.done = True
 
     def run(self, bot):

commit 79ca8895e369fe481009a60cb06b88759c570df1

Remove indent level, caused by d9a1b3d2ef184910924fca363d6675868fb36eb4

zirc/caps.py
@@ -23,10 +23,10 @@ class Caps(object):
                         self.args[cap] = args.split(',')
                     else:
                         self.args[cap] = None
-                if not self.availablecaps:
-                    self.bot.send("CAP END")
-                else:
-                    self.bot.send("CAP REQ :" + " ".join(self.availablecaps))
+            if not self.availablecaps:
+                self.bot.send("CAP END")
+            else:
+                self.bot.send("CAP REQ :" + " ".join(self.availablecaps))
         elif event.arguments[0] == "ACK" and not self.done:
             for cap in self.caps:
                 if hasattr(cap, "run"):