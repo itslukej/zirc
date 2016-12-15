fa535f66ded321613484aa4468318fc60c0193b6
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-12-15 10:37:20 -0500

    Add \n to the print's so we can prevent having two prints on the same line
    REF: https://travis-ci.org/wolfy1339/Python-IRC-Bot/jobs/183707985#L212

--- zirc/test.py
+++ zirc/test.py
@@ -6,12 +6,12 @@ from . import errors, util
 
 class TestCase(object):
     def start(self, log):
-        print_("Starting...", flush=True)
+        print_("Starting...\n", flush=True)
         log = log.split("\n")
         for line in log:
             try:
                 event = Event(line)
-                print_("Parsed line '{0}'".format(line), flush=True)
+                print_("Parsed line '{0}'\n".format(line), flush=True)
             except:
                 raise errors.InvalidLine(line)
 
@@ -19,18 +19,18 @@ class TestCase(object):
             args.update({k: getattr(event, k) for k in dir(event) if not k.startswith("__") and not k.endswith("__")})
 
             if hasattr(self, "on_all"):
-                print_("Attempting to run on_all...", flush=True)
+                print_("Attempting to run on_all...\n", flush=True)
                 util.function_argument_call(self.on_all, args)()
 
             text_type_func_name = "on_" + event.text_type.lower()
             if hasattr(self, text_type_func_name):
-                print_("Attempting to run {0}".format(text_type_func_name), flush=True)
+                print_("Attempting to run {0}\n".format(text_type_func_name), flush=True)
                 util.function_argument_call(getattr(self, text_type_func_name), args)()
 
             raw_type_func_name = "on_" + event.type.lower()
             if raw_type_func_name != text_type_func_name:
                 if hasattr(self, raw_type_func_name):
-                    print_("Attempting to run {0}".format(raw_type_func_name), flush=True)
+                    print_("Attempting to run {0}\n".format(raw_type_func_name), flush=True)
                     util.function_argument_call(getattr(self, raw_type_func_name), args)()
  
             if event.type == "PING":
@@ -38,7 +38,7 @@ class TestCase(object):
         print_("Done!", flush=True)
 
     def send(self, data):
-        print_("RESPONSE: '{0}'".format(data), flush=True)
+        print_("RESPONSE: '{0}'\n".format(data), flush=True)
         if hasattr(self, "on_send"):
             self.on_send(data)
 
