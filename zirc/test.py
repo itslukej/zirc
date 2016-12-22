f5f31b9ba07f919cd74c8bee748c84d7c9b27581
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-12-22 12:18:20 -0500

    Remove un-needed new-lines from the test case

--- zirc/test.py
+++ zirc/test.py
@@ -11,7 +11,7 @@ class TestCase(object):
         for line in log:
             try:
                 event = Event(line)
-                print_("Parsed line '{0}'\n".format(line), flush=True)
+                print_("Parsed line '{0}'".format(line), flush=True)
             except:
                 raise errors.InvalidLine(line)
 
@@ -19,20 +19,20 @@ class TestCase(object):
             args.update({k: getattr(event, k) for k in dir(event) if not k.startswith("__") and not k.endswith("__")})
 
             if hasattr(self, "on_all"):
-                print_("Attempting to run on_all...\n", flush=True)
+                print_("Attempting to run on_all...", flush=True)
                 util.function_argument_call(self.on_all, args)()
 
             text_type_func_name = "on_" + event.text_type.lower()
             if hasattr(self, text_type_func_name):
-                print_("Attempting to run {0}\n".format(text_type_func_name), flush=True)
+                print_("Attempting to run {0}".format(text_type_func_name), flush=True)
                 util.function_argument_call(getattr(self, text_type_func_name), args)()
 
             raw_type_func_name = "on_" + event.type.lower()
             if raw_type_func_name != text_type_func_name:
                 if hasattr(self, raw_type_func_name):
-                    print_("Attempting to run {0}\n".format(raw_type_func_name), flush=True)
+                    print_("Attempting to run {0}".format(raw_type_func_name), flush=True)
                     util.function_argument_call(getattr(self, raw_type_func_name), args)()
- 
+
             if event.type == "PING":
                 self.send("PONG :{0}".format(" ".join(event.arguments)))
         print_("Done!", flush=True)
