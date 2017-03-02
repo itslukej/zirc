c69c4c3b56bfbe61b73cb261c6d915f9664e5290
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-03-02 13:38:53 -0500

    Update __init__.py

--- zirc/__init__.py
+++ zirc/__init__.py
@@ -2,7 +2,10 @@ from .client import Client
 from .connection import Socket
 from .config import IRCConfig
 from .ext.sasl import Sasl
-from .ext.proxy import SOCKS4, SOCKS5, HTTP, Proxy
+try:
+    from .ext.proxy import SOCKS4, SOCKS5, HTTP, Proxy
+except ImportError:
+    pass
 from .ext.fifo import Fifo
 from .caps import Caps
 __version__ = '1.2.8'
