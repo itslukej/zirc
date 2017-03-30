ed594f8096ceb38bd63076dcb39a4c93b03e2995
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-03-30 13:26:51 -0400

    Fix tests by switching to freenode

--- tests/travis.py
+++ tests/travis.py
@@ -3,15 +3,13 @@ import zirc, time, sys, os
 class Bot(zirc.Client):
     def __init__(self):
         self.connection = zirc.Socket()
-        self.config = zirc.IRCConfig(host="irc.stuxnet.xyz", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[])
+        self.config = zirc.IRCConfig(host="irc.freenode.net", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[])
         self.connect(self.config)
         self.start()
 
     @staticmethod
-    def on_welcome(irc, event):
-        irc.send("JOIN #zirc")
-        time.sleep(1)
-        irc.privmsg("#zirc", "Testing script successful")
+    def on_endofmotd(irc, event):
+        irc.notice("#zirc", "Testing script successful")
         time.sleep(1)
         irc.send("QUIT :My work here is done")
         time.sleep(1)
