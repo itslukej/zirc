itslukej/zirc

commit 5b4b8fb20acc8e44431e8d4f5cc1826e47e75dcf

Use split and indexes to remove un used variables

zirc/event.py
@@ -65,24 +65,24 @@ class NickMask(six.text_type):
 
     @property
     def nick(self):
-        nick, sep, userhost = self.partition("!")
+        nick = self.split("!")[0]
         return nick
 
     @property
     def userhost(self):
-        nick, sep, userhost = self.partition("!")
+        userhost = self.split("!")[1]
         return userhost or None
 
     @property
     def host(self):
-        nick, sep, userhost = self.partition("!")
-        user, sep, host = userhost.partition('@')
+        userhost = self.split("!")[1]
+        host = userhost.split('@')[1]
         return host or None
 
     @property
     def user(self):
-        nick, sep, userhost = self.partition("!")
-        user, sep, host = userhost.partition('@')
+        userhost = self.split("!")[1]
+        user = userhost.split('@')[0]
         return user or None
 
     @classmethod

commit 84a0ae185f887dac0de17879892fea7c236f0274

Fix #33: An IndexError was being thrown in event
Issue introduced in 5b4b8fb20acc8e44431e8d4f5cc1826e47e75dcf

zirc/event.py
@@ -83,20 +83,20 @@ class NickMask(six.text_type):
 
     @property
     def userhost(self):
-        userhost = self.split("!")[1]
-        return userhost or None
+        userhost = self.split("!")
+        return userhost if len(userhost) else None
 
     @property
     def host(self):
         userhost = self.split("!")[1]
         host = userhost.split('@')[1]
-        return host or None
+        return host if len(userhost) else None
 
     @property
     def user(self):
         userhost = self.split("!")[1]
         user = userhost.split('@')[0]
-        return user or None
+        return user if len(userhost) else None
 
     @classmethod
     def from_group(cls, group):