b28ff4f3fa83d422b20d6d5c4b35397cbefffdd5
wolfy1339 <webmaster@wolfy1339.tk>
2017-04-13 15:35:29 -0400

    Use a bogus account I created just for this

--- tests/travis.py
+++ tests/travis.py
@@ -3,7 +3,7 @@ import zirc, time, sys, os
 class Bot(zirc.Client):
     def __init__(self):
         self.connection = zirc.Socket()
-        self.config = zirc.IRCConfig(host="irc.freenode.net", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[])
+        self.config = zirc.IRCConfig(host="irc.freenode.net", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[], caps=zirc.Caps(zirc.Sasl(username="zIRC", password=os.environ['irc_pass'])))
         self.connect(self.config)
         self.start()
 
