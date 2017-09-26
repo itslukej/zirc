91aac5b751049af25104ad4ded983946e128c966
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-09-25 23:20:03 -0400

    Update sasl.py

--- zirc/ext/sasl.py
+++ zirc/ext/sasl.py
@@ -1,6 +1,7 @@
 # SASL authentication for zirc
 
 import base64
+import pyxmpp2_scram as scram
 from ..errors import SASLError
 
 class Sasl(object):
@@ -11,10 +12,11 @@ class Sasl(object):
         self.password = password
         self.method = method
         self.retries = 0
+        self.sasl_scram_state = {'step': 'uninitialized'}
 
     def run(self, bot, args=None):
         if args is None:
-            mechanisms = ["EXTERNAL", "PLAIN"]
+            mechanisms = ["SCRAM-SHA256-PLUS", "SCRAM-SHA256", "EXTERNAL", "PLAIN"]
         else:
             mechanisms = args
         self.bot = bot
@@ -22,7 +24,42 @@ class Sasl(object):
         bot.listen(self.on_saslfailed, "saslfailed")
         bot.listen(self.on_saslsuccess, "saslsuccess")
         if self.method.upper() in mechanisms:
-            if self.method in ["plain", "external"]:
+            if self.method.startswith("scram-"):
+                step = self.sasl_scram_state['step']
+                try:
+                    if step == 'uninitialized':
+                        hash_name = self.mechanism[len('scram-'):]
+                        if hash_name.endswith('-plus'):
+                            hash_name = hash_name[:-len('-plus')]
+                        hash_name = hash_name.upper()
+                        if hash_name not in scram.HASH_FACTORIES:
+                            self.retries += 2
+                            return
+                        authenticator = scram.SCRAMClientAuthenticator(hash_name, channel_binding=False)
+                        self.sasl_scram_state['authenticator'] = authenticator
+                        client_first = authenticator.start({
+                            'username': self.sasl_username,
+                            'password': self.sasl_password,
+                         })
+                        self.sendSaslString(client_first)
+                        self.sasl_scram_state['step'] = 'first-sent'
+                    elif step == 'first-sent':
+                        client_final = self.sasl_scram_state['authenticator'].challenge(challenge)
+                        self.sendSaslString(client_final)
+                        self.sasl_scram_state['step'] = 'final-sent'
+                    elif step == 'final-sent':
+                        try:
+                            res = self.sasl_scram_state['authenticator'].finish(data)
+                        except scram.BadSuccessException:
+                            self.retries += 2
+                        else:
+                            self.sasl_scram_state['step'] = 'authenticated'
+                        else:
+                            assert False
+                    except scram.ScramException:
+                        bot.send('AUTHENTICATE *')
+                        self.retries +=  2
+            elif self.method in ["plain", "external"]:
                 bot.send("AUTHENTICATE " + self.method.upper())
             else:
                 raise SASLError("Not implemented yet")
@@ -40,7 +77,7 @@ class Sasl(object):
     def on_saslfailed(self, event):
         self.retries += 1
         if self.method == 'external':
-            if self.retries == 2:
+            if self.retries >= 2:
                 self.retries = 1
                 self.method = 'plain'
                 self.bot.send("AUTHENTICATE PLAIN")
