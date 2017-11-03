f071e7c193b2a793b0764dbe74c366bd2a8261e6
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2017-11-03 12:16:58 -0400

    Create scram.py

---
+++ zirc/ext/sasl/scram.py
@@ -0,0 +1,32 @@
+import pyxmpp2_scram as scram
+
+def doAuthenticateScramFirst(self, mechanism):
+    """Handle sending the client-first message of SCRAM auth."""
+    hash_name = mechanism[len('scram-'):]
+    if hash_name.endswith('-plus'):
+        hash_name = hash_name[:-len('-plus')]
+    hash_name = hash_name.upper()
+    if hash_name not in scram.HASH_FACTORIES:
+        self.retries += 2
+        return
+    authenticator = scram.SCRAMClientAuthenticator(hash_name, channel_binding=False)
+    self.sasl_scram_state['authenticator'] = authenticator
+    client_first = authenticator.start({
+        'username': self.sasl_username,
+        'password': self.sasl_password,
+        })
+    self.bot.send("AUTHENTICATE {0}".format(client_first))
+    self.sasl_scram_state['step'] = 'first-sent'
+
+def doAuthenticateScramChallenge(self, challenge):
+    client_final = self.sasl_scram_state['authenticator'].challenge(challenge)
+    self.bot.send("AUTHENTICATE {0}".format(client_final))
+    self.sasl_scram_state['step'] = 'final-sent'
+
+def doAuthenticateScramFinish(self, data):
+    try:
+        res = self.sasl_scram_state['authenticator'].finish(data)
+    except scram.BadSuccessException as e:
+        self.retries += 2
+    else:
+        self.sasl_scram_state['step'] = 'authenticated'
