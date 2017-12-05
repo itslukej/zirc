e4f73cca338eb8fb2b92af0dc7a40962bda1e4a6
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2017-12-05 14:46:46 -0500

    Make bot join an empty channel so notices work

--- tests/travis.py
+++ tests/travis.py
@@ -3,12 +3,12 @@ import zirc, time, sys, os
 class Bot(zirc.Client):
     def __init__(self):
         self.connection = zirc.Socket()
-        self.config = zirc.IRCConfig(host="irc.freenode.net", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=[], caps=zirc.Caps(zirc.Sasl(username="zIRC", password=os.environ['irc_pass'])))
+        self.config = zirc.IRCConfig(host="irc.freenode.net", port=6667, nickname="zIRC-CI-"+str(sys.version.split(" ")[0].replace(".", "-").replace("+", "")), ident="ci", realname="zIRC IRCP Library", channels=["##zirc-invalid"], caps=zirc.Caps(zirc.Sasl(username="zIRC", password=os.environ['irc_pass'])))
         self.connect(self.config)
         self.start()
 
     @staticmethod
-    def on_endofmotd(irc, event):
+    def on_join(irc, event):
         irc.notice("#zirc", "Testing script successful")
         time.sleep(1)
         irc.send("QUIT :My work here is done")
