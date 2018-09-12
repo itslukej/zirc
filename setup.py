2f1058974420677af0e539562436895566881870
wolfy1339 <webmaster@wolfy1339.com>
2018-09-12 13:13:29 -0400

    Bump release
    In order to remoe code from PyPi that wasn't working, and not on GitHub

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if version_info < (2, 7, 0) or (version_info[0] == 3 and version_info < (3, 2, 0
     exit(-1)
 
 setup(name='zirc',
-      version='1.2.9',
+      version='1.2.10',
       description='Python IRCP Library',
       long_description=long_description,
       url='https://github.com/itslukej/zirc',
