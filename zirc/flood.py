7fd2133e56ecc10fcc8c2ef02d2596b2bd48a422
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2021-05-24 19:49:44 -0400

    Update flood protection with implementation from Elios
    https://code.libertas.tech/bs/Eleos/-/blob/master/eleos.py#L505

--- zirc/flood.py
+++ zirc/flood.py
@@ -11,27 +11,33 @@ class floodProtect(object):
         self.canburst = True
 
     def queue_thread(self):
+        burstdone = False
         while True:
-            self.canburst = True if len(self.irc_queue) == 0 else self.canburst
-            if self.canburst:
-                for i in range(0, self.lines):
+            if len(self.irc_queue):
+                if not burstdone:
+                    for item in self.irc_queue[:self.lines]:
+                        try:
+                            connection, raw = item
+                        except Exception:
+                            self.irc_queue_running = False
+                            break
+                        connection.send(raw)
+                        self.irc_queue.remove(item)
+                    burstdone = True
+                if len(self.irc_queue) == 0:
+                    burstdone = False
+                else:
                     try:
                         connection, raw = self.irc_queue.pop(0)
                     except Exception:
                         self.irc_queue_running = False
                         break
                     connection.send(raw)
-                    sleep(self.sleep_time)
-                sleep(0.1) # Sleep here so we have an extra little buffer and so we can flush the queue
-                self.canburst = False
+                    time.sleep(self.sleep_time)
             else:
-                try:
-                    connection, raw = self.irc_queue.pop(0)
-                except Exception:
-                    self.irc_queue_running = False
-                    break
-                connection.send(raw)
-                sleep(1)
+                burstdone = False
+            time.sleep(0.2)
+
 
     def queue_add(self, connection, raw):
         self.irc_queue.append([connection, raw])
