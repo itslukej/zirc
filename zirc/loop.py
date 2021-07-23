e750d0815b445de0e5f25f53538be4b4d621c1aa
wolfy1339 <webmaster@wolfy1339.com>
2021-07-23 12:31:43 -0400

    Fix Callable syntax (2)

--- zirc/loop.py
+++ zirc/loop.py
@@ -5,13 +5,13 @@ from .event import Event
 
 class EventLoop(object):
     def __init__(self, recv_func: Callable[[], List[str]]):
-        self.jobs: OrderedDict[str, Callable[[Event]]] = OrderedDict()
+        self.jobs: OrderedDict[str, Callable[[Event], None]] = OrderedDict()
         self.cycles: int = 0
         self.current_job: Optional[str] = None
         self.break_loop = False
         self.recv_func = recv_func
 
-    def create_job(self, name: str, method: Callable[[Event]], do_thread: bool=False):
+    def create_job(self, name: str, method: Callable[[Event], None], do_thread: bool=False):
         self.jobs[name] = {"method": method, "thread": do_thread}
 
     def run(self):
